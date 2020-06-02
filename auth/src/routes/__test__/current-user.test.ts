281c04f1e139e907127760e9f74cd54991de01bc
Jesus Cervantes <impurepd@gmail.com>
2020-06-02 05:04:57 -0600

    breaking change

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -2,21 +2,21 @@ import request from 'supertest';
 import { app } from '../../app';
 
 it('should response with details about the current user', async () => {
-    const cookie = await global.getAuthCookie();
-    const response = await request(app)
-        .get('/api/users/current-user')
-        .set('Cookie', cookie)
-        .send({})
-        .expect(200)
+  const cookie = await global.getAuthCookie();
+  const response = await request(app)
+    .get('/api/users/current-user')
+    .set('Cookie', cookie)
+    .send({})
+    .expect(400);
 
-    expect(response.body.currentUser.email).toEqual('test@test.com')
+  expect(response.body.currentUser.email).toEqual('test@test.com');
 });
 
 it('should respond with null if not authenticated', async () => {
-    const response = await request(app)
-        .get('/api/users/current-user')
-        .send()
-        .expect(200)
+  const response = await request(app)
+    .get('/api/users/current-user')
+    .send()
+    .expect(200);
 
-    expect(response.body.currentUser).toEqual(null);
+  expect(response.body.currentUser).toEqual(null);
 });
